c38827aac906427c3da63c1e122b6d1762b3a271
zzzhang12 <zhangz_12@outlook.com>
2025-04-20 16:30:24 +0100

    feat: add a NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post.jsx'
 import classes from './PostsList.module.css'
-
+import NewPost
+ from './NewPost.jsx';
 function PostsList(){
     return(
-        <ul className={classes.posts}>
-            <Post author="Zhang" body="Learning React is fun!"/>
-            <Post author="Ying" body="Absolutely!"/>
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Zhang" body="Learning React is fun!"/>
+                <Post author="Ying" body="Absolutely!"/>
+            </ul>
+        </>
     )
 }
 
