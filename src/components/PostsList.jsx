6b0aacd84c387aa0b57b3e32a3fac3eecb03e79b
zzzhang12 <zhangz_12@outlook.com>
2025-04-20 22:01:32 +0100

    feat: add a submit handler to collect entered data and close modal overlay

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react';
 import Post from './Post.jsx'
 import classes from './PostsList.module.css'
 import NewPost from './NewPost.jsx';
 import Modal from './Modal.jsx';
 
 function PostsList({isPosting, onStopPosting}){
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function bodyChangeHandler(event){
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event){
-    setEnteredAuthor(event.target.value);
-  }
 
   return(
     <>
       {isPosting ? (<Modal onClose={onStopPosting}>
       <NewPost 
-        onBodyChange={bodyChangeHandler} 
-        onAuthorChange={authorChangeHandler}
         onCancel={onStopPosting}
       />
       </Modal>) : false
       }
     
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody}/>
         <Post author="Ying" body="Absolutely!"/>
       </ul>
     </>
