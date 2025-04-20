73d1cce2cf0bb45b439605db5a39cf126dfb4490
zzzhang12 <zhangz_12@outlook.com>
2025-04-20 01:09:11 +0100

    feat: try outputting dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,11 @@
+const names = ['Zhang', 'Ying']
+
 function Post(){
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+
     return(
         <div>
+            <p>{chosenName}</p>
             <p>Hello World!</p>
         </div>
     );
