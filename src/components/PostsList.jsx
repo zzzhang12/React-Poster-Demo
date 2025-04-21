144ca7838eb6ab474a5539d88960278f14d677a2
zzzhang12 <zhangz_12@outlook.com>
2025-04-21 23:48:31 +0100

    feat: show loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,14 @@ import Modal from "./Modal.jsx";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
-
+  const [isFetching, setIsFetching] = useState(false);
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
@@ -36,17 +38,22 @@ function PostsList({ isPosting, onStopPosting }) {
       ) : (
         false
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading Posts...</p>
         </div>
       )}
     </>
