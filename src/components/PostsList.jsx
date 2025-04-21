fcbf3c92dfaa2db201ed864c11c91fcf6213b1e1
zzzhang12 <zhangz_12@outlook.com>
2025-04-21 20:43:50 +0100

    feat: send a POST request to the backend when adding a post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,6 +8,13 @@ function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts',{
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     setPosts((existingData) => [postData, ...posts]);
   }
 
